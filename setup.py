e15597c64dcdc8185692899d75c17e61052daa61
Thibaut Havel <thibaut.havel@gmail.com>
2014-08-06 16:10:50 +0200

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synolopy',
-    version='0.1.0',
+    version='0.1.1',
     description='Synology Python API',
     author='thavel',
     packages=find_packages(),
